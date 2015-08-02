d12c4d91aac823ed82d5b7868250b5812f0eb5e8
Sean's computer <mipen@windowslive.com>
2015-08-02 22:46:52 +1200

    Mod help tab
    Adds mod help tab -
    Modders can define two new defs; HelpDef and HelpCategoryDef. These let
    modders add in small tutorials or the like to be accessed in-game

---
+++ DLL_Project/Defs/HelpDef.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CommunityCoreLibrary
+{
+    public class HelpDef : Def, IComparable
+    {
+        public HelpCategoryDef category;
+
+        public override void ResolveReferences()
+        {
+            base.ResolveReferences();
+            if (category == null)
+                Log.Warning(defName + " has null help category. This means it won't be displayed in the help menu");
+        }
+
+        public int CompareTo(object obj)
+        {
+            HelpDef d = obj as HelpDef;
+            if (d != null)
+            {
+                return d.label.CompareTo(label);
+            }
+            return 1;
+        }
+    }
+}
