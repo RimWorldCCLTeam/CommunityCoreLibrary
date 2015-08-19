8bfc5b3293a2c032776ed848e4ef8e39756313c1
ForsakenShell <cowles1e@cs.uregina.ca>
2015-08-19 02:11:42 -0600

    .

--- DLL_Project/Properties/AssemblyInfo.cs
+++ DLL_Project/Properties/AssemblyInfo.cs
@@ -12,23 +12,21 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+// Set ComVisible to false as we're not doing any COM stuff
 [assembly: ComVisible(false)]
 
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+// Need a GUID for the COM we aren't using
 [assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+// Versions information for the assembly:
 //
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
+//      Major
+//      Minor
+//      Build
 //      Revision
 //
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
+// Can leave some values to be auto-defined such as Build and Revision
+// For auto-defined values use "*", eg:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.11.5")]
 [assembly: AssemblyFileVersion("0.11.5")]
