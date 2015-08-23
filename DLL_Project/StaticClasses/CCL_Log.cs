19c2c91fe6b69a13548bad9733aec1f5a58d0635
urty5656 <urty5656@gmail.com>
2015-08-24 02:11:19 +0900

    Lazy one's convenient function

---
+++ DLL_Project/StaticClasses/CCL_Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace CommunityCoreLibrary
+{
+	internal static class CCL_Log
+	{
+		/// <summary>
+		/// Write a log => Community Core Library :: category(nullable) :: content
+		/// </summary>
+		public static void Message(string category, string content)
+		{
+			var builder = new StringBuilder();
+			builder.Append("Community Core Library:: ");
+
+			if (category != null)
+				builder.Append(category).Append(" :: ");
+
+			builder.Append(content);
+
+			Verse.Log.Message(builder.ToString());
+		}
+
+		/// <summary>
+		/// Write an error => Community Core Library :: category(nullable) :: content
+		/// </summary>
+		public static void Error(string category, string content)
+		{
+			var builder = new StringBuilder();
+			builder.Append("Community Core Library:: ");
+
+			if (category != null)
+				builder.Append(category).Append(" :: ");
+
+			builder.Append(content);
+
+			Verse.Log.Error(builder.ToString());
+		}
+	}
+}
