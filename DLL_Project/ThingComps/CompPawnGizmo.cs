27c52afe871af70472ea283ee8bbc4714292bd5f
urty5656 <urty5656@gmail.com>
2015-08-24 12:28:17 +0900

    Ranged weapon gizmo giver, extended Verb_Shoot

---
+++ DLL_Project/ThingComps/CompPawnGizmo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace CommunityCoreLibrary
+{
+	public class CompPawnGizmo : ThingComp
+	{
+        public override IEnumerable<Command> CompGetGizmosExtra()
+		{
+	        var comp = (parent as Pawn)?.equipment?.Primary?.AllComps.Find(s =>
+	        {
+		        if (!s.GetType().IsSubclassOf(typeof (CompRangedGizmoGiver)))
+		        {
+			        Log.Message(s + " not sub");
+			        return false;
+		        }
+
+		        var gizmoGiver = s as CompRangedGizmoGiver;
+		        return gizmoGiver != null && gizmoGiver.isRangedGiver;
+	        });
+
+	        if (comp == null)
+	        {
+				Log.Message("null");
+				yield break;
+	        }
+
+			Log.Message("gizmo");
+			foreach (var current in comp.CompGetGizmosExtra())
+				yield return current;
+		}
+	}
+}
