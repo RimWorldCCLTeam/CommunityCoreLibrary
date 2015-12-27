26676f4223b3a88fbe2f2c8446c4ea5ba7de38ea
Karel Kroeze <karel.kroeze@gmail.com>
2015-12-27 14:11:01 +0100

    Very simple first implementation of RawCode's method hooks

---
+++ DLL_Project/StaticClasses/Detours.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommunityCoreLibrary
+{
+    public static class Detours
+    {
+        public static unsafe bool TryDetourFromTo ( MethodInfo source, MethodInfo destination )
+        {
+            if( source == null || destination == null )
+                return false;
+
+            // get pointers
+            int Source_Base         = source     .MethodHandle.GetFunctionPointer ().ToInt32();
+            int Destination_Base    = destination.MethodHandle.GetFunctionPointer ().ToInt32();
+
+            // get offset
+            int offset_raw = Destination_Base - Source_Base;
+            uint* Pointer_Raw_Source = (uint*)Source_Base;
+
+            // insert jump to destination into source
+            *( Pointer_Raw_Source + 0 ) = 0xE9909090;
+            *( Pointer_Raw_Source + 1 ) = (uint)( offset_raw - 8 );
+            
+            // done!
+            return true;
+        }
+    }
+}
