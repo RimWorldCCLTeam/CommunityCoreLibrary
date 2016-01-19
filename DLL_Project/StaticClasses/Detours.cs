1db9f857dce35fb5db48992bb2b03bba4515b4e8
Karel Kroeze <karel.kroeze@gmail.com>
2016-01-19 19:36:49 +0100

    attempt at 64 bit detour code (highly likely to be incorrect)

--- DLL_Project/StaticClasses/Detours.cs
+++ DLL_Project/StaticClasses/Detours.cs
@@ -20,12 +20,20 @@ namespace CommunityCoreLibrary
         **/
         public static unsafe bool TryDetourFromTo ( MethodInfo source, MethodInfo destination )
         {
-            if( source == null || destination == null )
-                return false;
+            // error out on null arguments
+            if ( source == null )
+            {
+                throw new ArgumentNullException( nameof( source ) );
+            }
+
+            if ( destination == null )
+            {
+                throw new ArgumentNullException( nameof(destination) );
+            }
 
             // keep track of detours and spit out some messaging
-            string sourceString = source.DeclaringType.FullName + source.Name;
-            string destinationString = destination.DeclaringType.FullName + destination.Name;
+            string sourceString      = source.DeclaringType.FullName      + "." + source.Name;
+            string destinationString = destination.DeclaringType.FullName + "." + destination.Name;
 
             if ( detoured.Contains( sourceString ) )
             {
@@ -35,17 +43,41 @@ namespace CommunityCoreLibrary
             detoured.Add( sourceString );
             destinations.Add( destinationString );
 
-            // get pointers
-            int Source_Base         = source     .MethodHandle.GetFunctionPointer ().ToInt32();
-            int Destination_Base    = destination.MethodHandle.GetFunctionPointer ().ToInt32();
+            // are we running in a 32 or 64 bit process?
+            bool is64Bit = IntPtr.Size == 8;
 
-            // get offset
-            int offset_raw = Destination_Base - Source_Base;
-            uint* Pointer_Raw_Source = (uint*)Source_Base;
+            // RW will normally run in 32 bit, even if the OS/CPU are capable of running 64 bit.
+            // The major exception appears to be recent Macs (Yosemite 10.5)
+            if ( is64Bit )
+            {
+                // 64 bit code
+                // get pointers
+                long Source_Base         = source     .MethodHandle.GetFunctionPointer ().ToInt64();
+                long Destination_Base    = destination.MethodHandle.GetFunctionPointer ().ToInt64();
 
-            // insert jump to destination into source
-            *( Pointer_Raw_Source + 0 ) = 0xE9909090;
-            *( Pointer_Raw_Source + 1 ) = (uint)( offset_raw - 8 );
+                // get offset
+                long offset_raw = Destination_Base - Source_Base;
+                ulong* Pointer_Raw_Source = (ulong*) Source_Base;
+
+                // insert jump to destination into source
+                *( Pointer_Raw_Source + 0 ) = 0xE990909090909090;
+                *( Pointer_Raw_Source + 1 ) = (ulong)( offset_raw - 16 );
+            }
+            else
+            {
+                // 32 bit code
+                // get pointers
+                int Source_Base         = source     .MethodHandle.GetFunctionPointer ().ToInt32();
+                int Destination_Base    = destination.MethodHandle.GetFunctionPointer ().ToInt32();
+
+                // get offset
+                int offset_raw = Destination_Base - Source_Base;
+                uint* Pointer_Raw_Source = (uint*)Source_Base;
+
+                // insert jump to destination into source
+                *( Pointer_Raw_Source + 0 ) = 0xE9909090;
+                *( Pointer_Raw_Source + 1 ) = (uint)( offset_raw - 8 );
+            }
             
             // done!
             return true;
