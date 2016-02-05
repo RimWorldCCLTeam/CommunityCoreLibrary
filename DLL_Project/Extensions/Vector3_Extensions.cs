0593c9586ee5ff442e7a6ec80dfbfe1ad0077dfc
Karel Kroeze <karel.kroeze@gmail.com>
2016-02-06 00:01:47 +0100

    [fixes] I forgot to add a file to git again. Vector3_Extensions for sum, min and max on V3.

---
+++ DLL_Project/Extensions/Vector3_Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CommunityCoreLibrary
+{
+    public static class Vector3_Extensions
+    {
+        public static float Sum( this Vector3 vec )
+        {
+            return vec.x + vec.y + vec.z;
+        }
+
+        public static float Max( this Vector3 vec )
+        {
+            return Mathf.Max( vec.x, Mathf.Max( vec.y, vec.z ) );
+        }
+
+        public static float Min( this Vector3 vec )
+        {
+            return Mathf.Min( vec.x, Mathf.Min( vec.y, vec.z ) );
+        }
+
+        public static Vector3 Subtract( this Vector3 vec, float minus )
+        {
+            return new Vector3( vec.x - minus, vec.y - minus, vec.z - minus );
+        }
+    }
+}
