85290635d5caab2775c5369414767e92d268877d
Fluffy <fluffy.l2032@gmail.com>
2016-05-04 22:11:39 +0200

    [added]  - Actions are multicast, tweaked IntermodCommunication system to handle more like an event system where more then one action can hook into the same event. Unless I completely missed something and something actually depends on this behaviour?

--- DLL_Project/StaticClasses/InterModCommunication.cs
+++ DLL_Project/StaticClasses/InterModCommunication.cs
@@ -1,54 +1,57 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-
-using RimWorld;
 using Verse;
 
 namespace CommunityCoreLibrary
 {
-
     public static class InterModCommunication
     {
+        #region Fields
 
         private static Dictionary<string,Action>   handlers;
+
+        #endregion Fields
+
+        #region Methods
+
+        /// <summary>
+        /// Broadcasts a message to a handler.
+        /// </summary>
+        /// <param name="message">Message to handle.</param>
+        public static void BroadcastMessage( string message )
+        {
+            if ( handlers == null )
+            {
+                handlers = new Dictionary<string, Action>();
+            }
+            if ( !handlers.ContainsKey( message ) )
+            {
+                return;
+            }
+            handlers[message].Invoke();
+        }
 
         /// <summary>
         /// Registers a message handler.
         /// </summary>
         /// <param name="message">Message to handle.</param>
         /// <param name="callback">Action callback when the message is broadcast.</param>
-        public static void           RegisterForMessage( string message, Action callback )
+        public static void RegisterForMessage( string message, Action callback )
         {
-            if( handlers == null )
+            if ( handlers == null )
             {
                 handlers = new Dictionary<string, Action>();
             }
-            if( handlers.ContainsKey( message ) )
+            if ( handlers.ContainsKey( message ) )
             {
-                return;
+                handlers[message] += callback;
             }
             handlers.Add( message, callback );
         }
 
-        /// <summary>
-        /// Broadcasts a message to a handler.
-        /// </summary>
-        /// <param name="message">Message to handle.</param>
-        public static void          BroadcastMessage( string message )
-        {
-            if( handlers == null )
-            {
-                handlers = new Dictionary<string, Action>();
-            }
-            if( !handlers.ContainsKey( message ) )
-            {
-                return;
-            }
-            handlers[ message ].Invoke();
-        }
-
+        #endregion Methods
     }
-
 }
