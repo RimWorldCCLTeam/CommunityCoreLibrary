382f242e66d87e2e12efceab6dd1101f235e8fa4
ForsakenShell <cowles1e@cs.uregina.ca>
2016-05-13 14:12:35 -0600

    [adds] + IConfigurable for minimaps and overlays

---
+++ DLL_Project/Interfaces/IConfigurable.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+using UnityEngine;
+using Verse;
+
+
+namespace CommunityCoreLibrary
+{
+
+    public interface IConfigurable : IExposable
+    {
+
+        float                               DrawMCMRegion( Rect InRect );
+
+    }
+
+}
