a5a4ac01020416a634fe7d09978ae4ff3bcf2c52
Fluffy <fluffy.l2032@gmail.com>
2016-05-17 21:21:17 +0200

    [adds]  - lock toggle to allow drag/resize
    [removed]
     - checks and bounds on position/size of rect (sorry, removed the 'don't overlap devmode' stuff)

--- DLL_Project/MapComponents/MiniMapController.cs
+++ DLL_Project/MapComponents/MiniMapController.cs
@@ -1,32 +1,29 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-
-using RimWorld;
 using UnityEngine;
 using Verse;
 
 namespace CommunityCoreLibrary.MiniMap
 {
-
     public class MiniMapController : MapComponent
     {
-
         #region Fields
-        public static bool              visible = false;
 
         public static bool              dirty = true;
         public static bool              initialized = false;
-        public static Vector2           windowSize = new Vector2( 250f, 250f );
+        public static bool              visible = false;
         public static List<MiniMap>     visibleMiniMaps = new List<MiniMap>();
+        public static Vector2           windowSize = new Vector2( 250f, 250f );
 
         #endregion Fields
 
         #region Properties
 
-        private Window_MiniMap          GetWindow
+        private Window_MiniMap GetWindow
         {
             get
             {
@@ -36,15 +33,69 @@ namespace CommunityCoreLibrary.MiniMap
 
         #endregion Properties
 
-        #region Base Overrides
+        #region Methods
 
-        public override void            MapComponentOnGUI()
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            foreach ( var minimap in Controller.Data.MiniMaps )
+            {
+                #region Minimap Header
+
+                Scribe.EnterNode( minimap.miniMapDef.defName );
+
+                #endregion Minimap Header
+
+                var hidden = minimap.Hidden;
+                Scribe_Values.LookValue( ref hidden, "hidden" );
+
+                if ( Scribe.mode == LoadSaveMode.LoadingVars )
+                {
+                    minimap.Hidden = hidden;
+                }
+
+                #region Handle all MiniMap Overlays
+
+                foreach ( var overlay in minimap.overlayWorkers )
+                {
+                    #region Overlay Header
+
+                    Scribe.EnterNode( overlay.overlayDef.defName );
+
+                    #endregion Overlay Header
+
+                    hidden = overlay.Hidden;
+                    Scribe_Values.LookValue( ref hidden, "hidden" );
+
+                    if ( Scribe.mode == LoadSaveMode.LoadingVars )
+                    {
+                        overlay.Hidden = hidden;
+                    }
+
+                    #region Finalize Overlay
+
+                    Scribe.ExitNode();
+
+                    #endregion Finalize Overlay
+                }
+
+                #endregion Handle all MiniMap Overlays
+
+                #region Finalize Minimap
+
+                Scribe.ExitNode();
+
+                #endregion Finalize Minimap
+            }
+        }
+
+        public override void MapComponentOnGUI()
         {
             // No minimap
-            if( !visible )
+            if ( !visible )
             {
                 // Close the window if needed
-                if( GetWindow != null )
+                if ( GetWindow != null )
                 {
                     CloseWindow();
                 }
@@ -52,25 +103,22 @@ namespace CommunityCoreLibrary.MiniMap
             }
 
             // Initialize the map component
-            if( !initialized )
+            if ( !initialized )
             {
                 Initialize();
             }
 
             // Make sure the window is open
-            if( GetWindow == null )
+            if ( GetWindow == null )
             {
-                if( !OpenWindow() )
+                if ( !OpenWindow() )
                 {
                     return;
                 }
             }
 
-            // Update the window
-            UpdateWindow();
-
             // (Re-)sort minimaps
-            if( dirty )
+            if ( dirty )
             {
                 SortMiniMaps();
             }
@@ -79,53 +127,14 @@ namespace CommunityCoreLibrary.MiniMap
             UpdateMiniMaps();
         }
 
-        public override void ExposeData()
+        private void CloseWindow()
         {
-            base.ExposeData();
-            foreach( var minimap in Controller.Data.MiniMaps )
+            var window = GetWindow;
+            if ( window != null )
             {
-                #region Minimap Header
-                Scribe.EnterNode( minimap.miniMapDef.defName );
-                #endregion
-
-                var hidden = minimap.Hidden;
-                Scribe_Values.LookValue( ref hidden, "hidden" );
-
-                if( Scribe.mode == LoadSaveMode.LoadingVars )
-                {
-                    minimap.Hidden = hidden;
-                }
-
-                #region Handle all MiniMap Overlays
-                foreach( var overlay in minimap.overlayWorkers )
-                {
-                    #region Overlay Header
-                    Scribe.EnterNode( overlay.overlayDef.defName );
-                    #endregion
-
-                    hidden = overlay.Hidden;
-                    Scribe_Values.LookValue( ref hidden, "hidden" );
-
-                    if( Scribe.mode == LoadSaveMode.LoadingVars )
-                    {
-                        overlay.Hidden = hidden;
-                    }
-
-                    #region Finalize Overlay
-                    Scribe.ExitNode();
-                    #endregion
-                }
-                #endregion
-
-                #region Finalize Minimap
-                Scribe.ExitNode();
-                #endregion
+                Find.WindowStack.TryRemove( window );
             }
         }
-
-        #endregion
-
-        #region Initialization
 
         private void Initialize()
         {
@@ -137,15 +146,43 @@ namespace CommunityCoreLibrary.MiniMap
 
             // perform initial update for all overlays
             // do after initialization flag to avoid infinite loop.
-            foreach( var minimap in visibleMiniMaps )
+            foreach ( var minimap in visibleMiniMaps )
             {
                 minimap.Update();
             }
         }
 
-        #endregion
+        private bool OpenWindow()
+        {
+            var window = GetWindow;
+            if ( window != null )
+            {
+                return true;
+            }
 
-        #region Update Methods
+            window = new Window_MiniMap( Window_MiniMap.windowRect )
+            {
+                currentWindowRect = new Rect( Screen.width - windowSize.x, 0f, windowSize.y, windowSize.x )
+            };
+            if ( window == null )
+            {
+                CCL_Log.Error( "Unable to create Window_MiniMap", "MiniMap" );
+                return false;
+            }
+            Find.WindowStack.Add( window );
+            return true;
+        }
+
+        private void SortMiniMaps()
+        {
+            // keep in mind that default sort ordering is FALSE > TRUE (which makes sense given the binary representation).
+            visibleMiniMaps = Controller.Data.MiniMaps.Where( overlay => !overlay.Hidden )
+                               .OrderBy( overlay => overlay.miniMapDef.alwaysOnTop )
+                               .ThenBy( overlay => overlay.miniMapDef.drawOrder )
+                               .ToList();
+
+            dirty = false;
+        }
 
         private void UpdateMiniMaps()
         {
@@ -160,95 +197,6 @@ namespace CommunityCoreLibrary.MiniMap
             }
         }
 
-        private void UpdateWindow()
-        {
-            var window = GetWindow;
-            if( window == null )
-            {
-                return;
-            }
-
-            if( Prefs.DevMode )
-            {   // Adjust window position to accomodate the dev icon bar on smaller resolutions
-                var devIconBarStart = Screen.width * 0.6666667f;
-                int num = 6;
-                if (Game.Mode == GameMode.MapPlaying)
-                {
-                    num += 2;
-                }
-                var devIconBarSize = ( num * 28.0 - 4.0 + 1.0 );
-                var devIconBarRight = devIconBarStart + devIconBarSize;
-                var windowX = window.currentWindowRect.x;
-                if( devIconBarRight >= windowX )
-                {
-                    Window_MiniMap.windowRect.y = 0f;
-                    var offsetFromTop = 3f + 25f + 3f;
-                    if(
-                        ( Game.Mode == GameMode.MapPlaying )&&
-                        ( Game.GodMode )&&
-                        ( devIconBarStart + 60f >= windowX )
-                    )
-                    {
-                        offsetFromTop+= 15f + 3f;
-                    }
-                    Window_MiniMap.windowRect.y += offsetFromTop;
-                }
-            }
-            if( window.currentWindowRect != Window_MiniMap.windowRect )
-            {
-                window.currentWindowRect = Window_MiniMap.windowRect;
-            }
-        }
-
-        #endregion
-
-        #region Open/Close Window
-
-        private void CloseWindow()
-        {
-            var window = GetWindow;
-            if( window != null )
-            {
-                Find.WindowStack.TryRemove( window );
-            }
-        }
-
-        private bool OpenWindow()
-        {
-            var window = GetWindow;
-            if( window != null )
-            {
-                return true;
-            }
-
-            Window_MiniMap.windowRect = new Rect( Screen.width - windowSize.x, 0f, windowSize.y, windowSize.x );
-            window = new Window_MiniMap( Window_MiniMap.windowRect );
-            if( window == null )
-            {
-                CCL_Log.Error( "Unable to create Window_MiniMap", "MiniMap" );
-                return false;
-            }
-            Find.WindowStack.Add( window );
-            return true;
-        }
-
-        #endregion
-
-        #region Sort MiniMaps
-
-        private void SortMiniMaps()
-        {
-            // keep in mind that default sort ordering is FALSE > TRUE (which makes sense given the binary representation).
-            visibleMiniMaps = Controller.Data.MiniMaps.Where( overlay => !overlay.Hidden )
-                               .OrderBy( overlay => overlay.miniMapDef.alwaysOnTop )
-                               .ThenBy( overlay => overlay.miniMapDef.drawOrder )
-                               .ToList();
-
-            dirty = false;
-        }
-
-        #endregion
-
+        #endregion Methods
     }
-
 }
