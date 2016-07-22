32f51eb290a7f56b5c9cb2b715bf8dc68627938f
ForsakenShell <cowles1e@cs.uregina.ca>
2016-07-21 21:40:19 -0600

    [changes] + Bumps version to v0.14.1 + Adds list of compatible RimWorld builds to mod description + Changes target version to minimum supported RimWorld build + Updated the def description for ModHelperDef regarding how version numbers are treated
    Rebuilds DLLs for new release

--- DLL_Project/Version/Version.cs
+++ DLL_Project/Version/Version.cs
@@ -9,7 +9,7 @@ namespace CommunityCoreLibrary
         #region Instance Data
 
         private static System.Version       versionMin = new System.Version( "0.14.0" );
-        private const string                versionCurrentInt = "0.14.0.1";
+        private const string                versionCurrentInt = "0.14.1";
 
         private static System.Version       versionCurrent;
 
